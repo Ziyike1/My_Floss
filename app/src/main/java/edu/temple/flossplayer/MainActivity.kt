ddf8dd4f222527aea7dc4ad836ec3ab09d63d702
Ziyike1 <tul51428@temple.edu>
2023-11-12 17:03:10 -0500

    add the search button and set up the api things

--- app/src/main/java/edu/temple/flossplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/flossplayer/MainActivity.kt
@@ -1,11 +1,27 @@
 package edu.temple.flossplayer
 
+import BookListFragment
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.ImageButton
+import android.widget.SearchView
 import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +39,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val searchButton = findViewById<ImageButton>(R.id.searchButton)
+        searchButton.setOnClickListener {
+            onSearchRequested()
+        }
 
         // Use Back gesture to clear selected book
         backPressCallback = object: OnBackPressedCallback(true) {
@@ -40,7 +60,9 @@ class MainActivity : AppCompatActivity() {
 
         onBackPressedDispatcher.addCallback(this, backPressCallback)
 
-        bookViewModel.setBookList(getBookList())
+        bookViewModel.getBookList().observe(this, Observer { books ->
+            bookViewModel.setBookList(books)
+        })
 
         // If we're switching from one container to two containers
         // clear BookPlayerFragment from container1
@@ -85,6 +107,52 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            performSearch(query)
+        }
+    }
+
+    private fun performSearch(query: String?) {
+        query?.let {
+            Thread {
+                try {
+                    val url = URL("https://kamorris.com/lab/flossplayer/searchbooks.php?query=$query")
+                    val urlConnection = url.openConnection() as HttpURLConnection
+                    try {
+                        val inStream = BufferedInputStream(urlConnection.inputStream)
+                        val reader = BufferedReader(InputStreamReader(inStream))
+                        val result = StringBuilder()
+                        reader.forEachLine { line ->
+                            result.append(line)
+                        }
+                        handleResult(result.toString())
+                    } finally {
+                        urlConnection.disconnect()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun handleResult(jsonResult: String) {
+        val gson = Gson()
+        val type = object : TypeToken<List<Book>>() {}.type
+        val books: List<Book> = gson.fromJson(jsonResult, type)
+        Handler(Looper.getMainLooper()).post {
+            updateUI(books)
+        }
+    }
+
+    private fun updateUI(books: List<Book>) {
+        val fragment = supportFragmentManager.findFragmentById(R.id.container1) as? BookListFragment
+        fragment?.updateBooks(books)
+    }
+
 
     private fun getBookList() : BookList {
         val bookList = BookList()
